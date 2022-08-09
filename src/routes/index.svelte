5a49422cb74f162531aa463bd46b7ebbb718af66
Steve <stevedsimkins@gmail.com>
2022-08-08 20:06:57 -0400

    updated favicon and meta

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,26 @@
     @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;800&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@400;500&display=swap');
   </style>
+  <title>Steve Simkins</title>
+  <meta
+    name="description"
+    content="Community Manager, Web3 Developer, Technical Writer"/>
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.stevedsimkins.dev/">
+  <meta property="og:title" content="Steve Simkins">
+  <meta property="og:description" content="Community Manager, Web3 Developer, Technical Writer">
+  <meta property="og:image" content="https://stevedsimkins.mypinata.cloud/ipfs/QmXVuzKtgt4tS8KCbRVAqdRXVMCJybNUP3BgFQcWsefKtH">
+  
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@stevedsimkins" />
+  <meta name="twitter:creator" content="@stevedsimkins" />
+  <meta property="og:url" content="https://www.stevedsimkins.dev/" />
+  <meta property="og:title" content="Steve Simkins" />
+  <meta property="og:description" content="Community Manager, Web3 Developer, Technical Writer" />
+  <meta property="og:image" content="https://stevedsimkins.mypinata.cloud/ipfs/QmXVuzKtgt4tS8KCbRVAqdRXVMCJybNUP3BgFQcWsefKtH" />
+
 </script:head>
 
 <script>
